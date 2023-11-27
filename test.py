6f6ba442eb863821fcc977a6fd2857c38e1cf42d
183akshay <sharmahoney398@gmail.com>
2023-11-28 00:05:55 +0530

    learned till function from cheatsheet

---
+++ test.py
@@ -0,0 +1,27 @@
+#https://www.pythoncheatsheet.org/
+
+# match [1,2,3]:
+    
+#     case [a,b,c]:
+#         print(a,b,c)
+#     case [a,b,c]:
+#         print(a,b,c,"is here")
+
+# for i in [1, 2, 3, 4, 5]:
+#     if i == 3:
+#         pass
+# else:
+#     print("only executed when no item is equal to 3")
+
+# def fun(name, age=20):
+#     print(f"name is {name} and age is {age}")
+    
+# fun(age=45, name="akshay")
+
+# a = lambda x: x**2 #treat a as a function now
+# print(a(1))
+
+# def walk():
+#     return print("walking")
+
+# print(walk())
